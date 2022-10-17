a94857d49c8202940bca6b7bc12905deda26a119
qudwls5771 <qudwls5771@naver.com>
2022-10-17 18:25:55 +0900

    회원목록 추가

--- Team_pro_ex/src/main/java/com/example/team_pro_ex/Entity/member/Member.java
+++ Team_pro_ex/src/main/java/com/example/team_pro_ex/Entity/member/Member.java
@@ -68,7 +68,6 @@ public class Member extends member_BaseEntity {
     @Column(name = "member_pet_T", length = 20)
     private String petT; //--펫 종류
 
-    @Pattern(regexp = "(?=.*[0-9])(?=.*\\W)(?=\\S+$).{10}", message = "애견,애묘의 출생일은 예)20220901")
     @Column(name = "member_pet_D")
     private String petD; //-- 펫 출생
 

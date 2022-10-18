fe9875c4d1f76a819ba09102190aabfe57848ea2
kcas1005 <wltjdwls9@naver.com>
2022-10-18 09:07:17 +0900

    병진이꺼 머지

--- shop/src/main/java/com/shop/config/WebMvcConfig.java
+++ shop/src/main/java/com/shop/config/WebMvcConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
+    //이렇게 쓰기는 불편해서
     @Value("${uploadPath}")
     String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                //이 문자로 대체하겠다.
         registry.addResourceHandler("/images/**")
                 .addResourceLocations(uploadPath);
     }
--- shop/src/main/java/com/shop/service/CartService.java
+++ shop/src/main/java/com/shop/service/CartService.java
@@ -34,30 +34,36 @@ public class CartService {
 
     //member에서 id를 읽어와  cart객체를 얻어온다.
     //cart객체를 이용하여 CartItem를 넣는다.
-    public Long addCart(CartItemDto cartItemDto, String email){
-
+    public Long addCart(CartItemDto cartItemDto, String email){  //String email => member email
+        // Item에서 아이템 아이디로 찾는다.
         Item item = itemRepository.findById(cartItemDto.getItemId())
+                // orElse 는 default 값을 주는것이다 value 값이 null 일 때는
+                // default value 를 넣어서 사용하고 null 이 아닐 때는 해당 value 를 그대로 사용한다.
                 .orElseThrow(EntityNotFoundException::new);
+        //Member에서 email로 member.id를 찾는다.
         Member member = memberRepository.findByEmail(email);
-
+        // cart에서 member.id를 찾고 cart(member)가 null일 경우 cart를 생성한다.
         Cart cart = cartRepository.findByMemberId(member.getId());
         if(cart == null){
             cart = Cart.createCart(member);
             cartRepository.save(cart);
         }
-
+        // cartitem에서 cart.id와 item.id를 찾는다.
         CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
 
+        //cartitem이 null이 아닐경우 saveCartItem.getId()로 리턴한다.
         if(savedCartItem != null){
             savedCartItem.addCount(cartItemDto.getCount());
             return savedCartItem.getId();
-        } else {
+        } else { // null일 경우 cartItem(cart, item, cartItemDto.getCount())을 저장한다.
             CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
             cartItemRepository.save(cartItem);
             return cartItem.getId();
         }
     }
-
+    // @Transactional은 클래스나 메서드에 붙여줄 경우, 해당 범위 내 메서드가 트랜잭션이 되도록 보장해준다.
+    //선언적 트랜잭션이라고도 하는데, 직접 객체를 만들 필요 없이 선언만으로도 관리를 용이하게 해주기 때문.
+    //특히나 SpringBoot에서는 선언적 트랜잭션에 필요한 여러 설정이 이미 되어있는 탓에, 더 쉽게 사용할 수 있다.
     @Transactional(readOnly = true)
     public List<CartDetailDto> getCartList(String email){
 
